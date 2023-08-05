239ae26b2b793e373ce137cc44f8ca46236a1626
soutovp <fernandoandradesouto@hotmail.com>
2023-08-05 11:48:42 -0300

    bla

--- src/Infra/http/express/index.ts
+++ src/Infra/http/express/index.ts
@@ -2,12 +2,18 @@ import express, { Express, Request, Response } from 'express'
 import { CreateRouteUseCase } from '../../../Application/create-route.use-case'
 import { RouteInMemoryRepository } from '../../db/route-in-memory.repository'
 const app: Express = express()
-
+const port = process.env.PORT || 3000
 const routeRepo = new RouteInMemoryRepository()
+
+app.use(express.json())
 
 app.post('/routes', async (req: Request, res: Response) => {
 
 	const createUseCase = new CreateRouteUseCase(routeRepo)
 	const output = await createUseCase.execute(req.body)
 	res.status(201).json(output)
+})
+
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
 })
